84bd5c92ef5b67dbd858e4698f10ef410a50a9a2
Kapil Kaisare <kapil@kaisare.in>
2022-05-22 11:10:34 -0500

    Construct and render minefield.

--- src/components/ms-minefield/ms-minefield.tsx
+++ src/components/ms-minefield/ms-minefield.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, h } from '@stencil/core';
+import { Component, Prop, Watch, h } from '@stencil/core';
 import { format } from '../../utils/utils';
 
 const range = (start, end) => {
@@ -7,43 +7,80 @@ const range = (start, end) => {
   return ranged;
 };
 
-const plantMine = () => (Math.floor(Math.random() * 10) > 5);
-const makeBlueprint = (rows, columns) => range(0, rows).map(() => range(0, columns).map(plantMine));
-const findMinedNeighbors = (blueprint, rowCount, colCount) => {
-  const minedNeighbors = [];
-  blueprint.forEach((row, rowIdx) => {
-    minedNeighbors[rowIdx] = [];
-    row.forEach((mined, colIdx) => {
-      minedNeighbors[rowIdx][colIdx] = [
-        (rowIdx !== 0 && colIdx !== 0 && blueprint[rowIdx - 1][colIdx - 1]),
-        (rowIdx !== 0 && blueprint[rowIdx - 1][colIdx]),
-        (rowIdx !== 0 && colIdx !== colCount - 1 && blueprint[rowIdx - 1][colIdx + 1]),
-        (colIdx !== 0 && blueprint[rowIdx][colIdx - 1]),
-        (colIdx !== colCount -1 && blueprint[rowIdx][colIdx + 1]),
-        (rowIdx !== rowCount - 1 && colIdx !== 0 && blueprint[rowIdx + 1][colIdx - 1]),
-        (rowIdx !== rowCount - 1 && blueprint[rowIdx + 1][colIdx]),
-        (rowIdx != rowCount - 1 && colIdx !== colCount - 1 && blueprint[rowIdx + 1][colIdx + 1])
-      ].reduce((prev, acc) => acc + (prev ? 1 : 0), 0);
-    });
-  });
-  return minedNeighbors;
-};
-
 @Component({
   tag: 'ms-minefield',
   styleUrl: 'ms-minefield.css',
   shadow: true,
 })
 export class MsMinefield {
+  private blueprint: Array<Array<Boolean>>;
+  private mineCounts: Array<Array<number>>;
+
   @Prop() rows: number;
   @Prop() columns: number;
 
-  render() {
-    const mineBlueprint = makeBlueprint(this.rows, this.columns);
-    // console.log(mineBlueprint);
-    const minedNeighbors = findMinedNeighbors(mineBlueprint, this.rows, this.columns);
-    // console.log(minedNeighbors);
+  @Watch('rows')
+  @Watch('columns')
+  connectedCallback() {
+    this.makeBlueprint();
+    this.determineMineCounts();
+  }
 
-    return <div></div>;
+  render() {
+    const renderedMinefield = this.renderBlueprint();
+    return <div>{renderedMinefield}</div>;
+  }
+
+  private makeBlueprint() {
+    this.blueprint = range(0, this.rows)
+    .map(() => range(0, this.columns)
+      .map(() => (Math.floor(Math.random() * 10) > 5))
+    );
+  }
+
+  private withNeighborIndices(row, col, pickFn) {
+    return [
+      (row !== 0 && col !== 0) ? pickFn.call(this, row - 1, col - 1) : null,
+      (row !== 0) ? pickFn.call(this, row - 1, col) : null,
+      (row !== 0 && col !== this.columns - 1) ? pickFn.call(this, row - 1, col - 1) : null,
+      (col !== 0) ? pickFn.call(this, row, col - 1) : null,
+      (col !== this.columns -1) ? pickFn.call(this, row, col + 1) : null,
+      (row !== this.rows - 1 && col !== 0) ? pickFn.call(this, row + 1, col - 1) : null,
+      (row !== this.rows - 1) ? pickFn.call(this, row + 1, col) : null,
+      (row !== this.rows - 1 && col !== this.columns - 1) ? pickFn.call(this, row + 1, col + 1) : null
+    ];
+  }
+
+  private determineMineCounts() {
+    this.mineCounts = this.blueprint
+    .map((row, rowIdx) => row
+      .map((col, colIdx) => {
+        const neighbors = this.withNeighborIndices(
+          rowIdx, colIdx,
+          (r, c) => { return this.blueprint[r][c]; }
+        );
+        return neighbors.reduce((acc, current) => acc + ((current !== null && current) ? 1 : 0), 0);
+      })
+    );
+  }
+
+  private renderBlueprint() {
+    const rows = this.blueprint.map((row, rowIdx) => {
+      return <tr>
+        {this.renderColumns(row, rowIdx)}
+      </tr>;
+    });
+    return <table><tbody>{rows}</tbody></table>;
+  }
+
+  private renderColumns(row, rowIdx) {
+    return row.map(
+      (column, colIdx) => <td>
+        <ms-cell
+          mined={column}
+          minedNeighbors={this.mineCounts[rowIdx][colIdx]}
+        ></ms-cell>
+      </td>
+    );
   }
 }
